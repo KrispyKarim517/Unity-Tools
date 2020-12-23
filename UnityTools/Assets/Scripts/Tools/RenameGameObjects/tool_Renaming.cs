c91279193299cb933e13695a9206ceaf1f500afa
KrispyKarim517 <39722911+KrispyKarim517@users.noreply.github.com>
2020-12-22 22:15:40 -0800

    Added close window to rename tool

--- UnityTools/Assets/Scripts/Tools/RenameGameObjects/tool_Renaming.cs
+++ UnityTools/Assets/Scripts/Tools/RenameGameObjects/tool_Renaming.cs
@@ -154,6 +154,7 @@ public class tool_Renaming : EditorWindow
                     obj.name = newGameObjectNames[i].ToString();
                     i += 1;
                 }
+                Close();
             }
         }
     }
