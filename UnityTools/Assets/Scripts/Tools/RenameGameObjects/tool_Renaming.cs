1fdcb554f5192e8a9a86eee47641f8492e1b1cab
KrispyKarim517 <39722911+KrispyKarim517@users.noreply.github.com>
2020-12-22 19:53:16 -0800

    added prefix and suffix

--- UnityTools/Assets/Scripts/Tools/RenameGameObjects/tool_Renaming.cs
+++ UnityTools/Assets/Scripts/Tools/RenameGameObjects/tool_Renaming.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-//[CanEditMultipleObjects]
 public class tool_Renaming : EditorWindow
 {
     public float removeButtonHorizontalPos = 0f;
     public float removeButtonVerticalPos = 500f;
     private GUIStyle boxStyle = new GUIStyle();
     private string textFormatting;
+    private string prefix = "";
+    private bool prefixAdded = false;
+    private string suffix = "";
+    private bool suffixAdded = false;
+    HashSet<Object> uniqueCheck = new HashSet<Object>();
+    Vector2 scrollPosition;
 
+
+
+    //Creates a custom unity window
     [MenuItem("Tools/Rename GameObjects")]
     private static void ToolWindow()
     {
         GetWindow<tool_Renaming>("Rename GameObjects");
     }
 
+
+    
+    //Allows objects from hierarchy to be dragged and dropped into custom window
     private static object[] DragDropArea()
     {
         EventType dragEvent = Event.current.type;
@@ -33,40 +44,91 @@ public class tool_Renaming : EditorWindow
         return null;
     }
 
-    private void OnGUI()
+
+
+    //Handles text formatting of dragged objects
+    private void DrawGameObjectNames()
     {
-        GUILayout.Label("Click & Drag GameObjects Here");
-        GUILayout.BeginArea(new Rect(0f, (Screen.height/1.8f), 100, 100));
         var removeObjects = GUILayout.Button("Remove All", GUILayout.Width(100f));
-        GUILayout.EndArea();
-        GUILayout.Box("Original\n--------\n"+textFormatting, GUILayout.Width(Screen.width), GUILayout.Height(Screen.height / 4));
-        GUILayout.Box("New\n-----\n", GUILayout.Width(Screen.width), GUILayout.Height(Screen.height / 4));
         if (removeObjects)
         {
             textFormatting = "";
+            uniqueCheck.Clear();
         }
-
-        var selectedObjects = DragDropArea();
-        if (selectedObjects == null)
+        if (DragDropArea() == null)
         {
             return;
         }
         else
         {
-            foreach (GameObject obj in selectedObjects)
+            foreach (GameObject obj in DragDropArea())
             {
-                textFormatting += obj.name.ToString() + "\n";
+                if (!uniqueCheck.Contains(obj))
+                {
+                    textFormatting += obj.name.ToString() + "\n";
+                    uniqueCheck.Add(obj);
+                }
                 //obj.name = "newName";
             }
         }
-        Debug.Log(textFormatting);
+    }
+
+    
+
+    //
+    private void DrawPrefixSuffix()
+    {
+        if (GUILayout.Button("Confirm", GUILayout.Width(100f)))
+        {
+            textFormatting = "";
+            foreach (GameObject obj in uniqueCheck)
+            {
+                textFormatting += prefix + obj.name.ToString() + suffix + "\n";
+            }
+        }
+    }
+
+
+
+    //Manages custom window interface display
+    private void OnGUI()
+    {
+        GUILayout.Label("Click & Drag GameObjects Here\n-------------------------------");
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(Screen.width), GUILayout.Height(100f));  
+        GUILayout.Label(textFormatting);
+        GUILayout.EndScrollView();
+        GUILayout.Label("-------------------------------\n");
+        DrawGameObjectNames();
+        prefix = EditorGUILayout.TextField("Add Prefix: ", prefix);
+        if (prefix != "" && !prefix.Contains(" "))
+        {
+            prefixAdded = true;
+            DrawPrefixSuffix();
+        }
+        else
+        {
+            prefixAdded = false;
+        }
+        suffix = EditorGUILayout.TextField("Add Suffix: ", suffix);
+        if (suffix != "" && !suffix.Contains(" "))
+        {
+            suffixAdded = true;
+            DrawPrefixSuffix();
+        }
+        else
+        {
+            suffixAdded = false;
+        }
     }
 }
 
 /*
 Notes:
 
-DragAndDrop.AcceptDrag();
+[CanEditMultipleObjects]
+
+Repaint();
+
 Selection.gameObjects[0].name.ToString()
 
     private string selectedGameObjects()
@@ -85,4 +147,17 @@ Selection.gameObjects[0].name.ToString()
         //{
         //    textFormatting = "";
         //}
+
+
+
+                    foreach (GameObject obj in DragAndDrop.objectReferences)
+                {
+                    uniqueCheck.Add(obj);
+                }
+
+
+GUILayout.BeginArea(new Rect(0f, (Screen.height / 1.8f), 100, 100));
+GUILayout.Box("Original\n--------\n" + textFormatting, GUILayout.Width(Screen.width), GUILayout.Height(Screen.height / 4));
+GUILayout.Box("New\n-----\n", GUILayout.Width(Screen.width), GUILayout.Height(Screen.height / 4));
+GUILayout.EndArea();
      */
