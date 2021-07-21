1108ae6339898bce8518e677ca4cf5ddd8f32b2c
KrispyKarim517 <39722911+KrispyKarim517@users.noreply.github.com>
2021-07-20 22:46:22 -0700

    Build Analyzer Window Created

---
+++ UnityTools/Assets/KrispyKarim/Krispy Build Analyzer/Scripts/tool_KrispysBuildAnalyzer.cs
@@ -0,0 +1,44 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+namespace KrispyBuildAnalyzer
+{
+    public class tool_KrispysBuildAnalyzer : EditorWindow
+    {
+        #pragma warning disable CS0649
+        [SerializeField]
+        public GUISkin darkTheme, lightTheme; //Window color theme
+        #pragma warning restore CS0649
+
+
+
+        //Creates custom unity window
+        [MenuItem("Tools/Krispy's Build Analyzer")]
+        private static void ToolWindow()
+        {
+            GetWindow<tool_KrispysBuildAnalyzer>("Krispy's Build Analyzer");
+        }
+
+
+
+        private void OnGUI()
+        {
+            //Sets the minimum size of the window
+            this.minSize = new Vector2(Screen.currentResolution.width / 4, Screen.currentResolution.height / 4);
+            
+
+            //Sets color theme based on unity theme
+            if (EditorGUIUtility.isProSkin == true)
+            {
+                GUI.skin = darkTheme; //If unity is in dark mode
+            }
+            else
+            {
+                GUI.skin = lightTheme; //If unity is in light mode
+            }
+        }
+    }
+}
+#endif
