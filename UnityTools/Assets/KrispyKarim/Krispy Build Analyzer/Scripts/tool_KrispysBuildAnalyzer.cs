5a5d5596ea976af6f057041c9ff7c7333a2a29fc
KrispyKarim517 <39722911+KrispyKarim517@users.noreply.github.com>
2021-07-22 14:24:53 -0700

    GUI Design

--- UnityTools/Assets/KrispyKarim/Krispy Build Analyzer/Scripts/tool_KrispysBuildAnalyzer.cs
+++ UnityTools/Assets/KrispyKarim/Krispy Build Analyzer/Scripts/tool_KrispysBuildAnalyzer.cs
@@ -27,7 +27,7 @@ namespace KrispyBuildAnalyzer
         {
             //Sets the minimum size of the window
             this.minSize = new Vector2(Screen.currentResolution.width / 4, Screen.currentResolution.height / 4);
-            
+            EditorGUIUtility.labelWidth = 75f;
 
             //Sets color theme based on unity theme
             if (EditorGUIUtility.isProSkin == true)
@@ -38,6 +38,60 @@ namespace KrispyBuildAnalyzer
             {
                 GUI.skin = lightTheme; //If unity is in light mode
             }
+
+            //Title
+            GUILayout.Label("Build Analyzer", GUILayout.Width(position.width), GUILayout.Height(75));
+
+            //Tabs
+            GUILayout.BeginArea(new Rect(0, 80, position.width / 3, position.height / 2));
+            //General Tab
+            if (GUILayout.Button("General", GUILayout.Width(position.width / 4), GUILayout.Height(position.height / 10)))
+            {
+                Debug.Log("Working");
+            }
+            //Size Data Tab
+            if (GUILayout.Button("Size Data", GUILayout.Width(position.width / 4), GUILayout.Height(position.height / 10)))
+            {
+                Debug.Log("Working");
+            }
+            //Assets Tab
+            if (GUILayout.Button("Assets", GUILayout.Width(position.width / 4), GUILayout.Height(position.height / 10)))
+            {
+                Debug.Log("Working");
+            }
+            //Project Settings Tab
+            if (GUILayout.Button("Project Settings", GUILayout.Width(position.width / 4), GUILayout.Height(position.height / 10)))
+            {
+                Debug.Log("Working");
+            }
+            GUILayout.EndArea();
+
+            //File Options
+            GUILayout.BeginArea(new Rect(0, position.height - 50, position.width / 12, 100));
+            //Open Player Log
+            if (GUILayout.Button("Open\nPlayer\nLog", GUILayout.Width(position.width / 12), GUILayout.Height(50)))
+            {
+                Debug.Log("open the player log file");
+            }
+            GUILayout.EndArea();
+            GUILayout.BeginArea(new Rect(position.width / 12, position.height - 50, position.width / 12, 100));
+            //Open Editor Log
+            if (GUILayout.Button("Open\nEditor\nLog", GUILayout.Width(position.width / 12), GUILayout.Height(50)))
+            {
+                Debug.Log("open the editor log file");
+            }
+            GUILayout.EndArea();
+            GUILayout.BeginArea(new Rect(position.width / 6, position.height - 50, position.width / 12, 100));
+            //Export Data
+            if (GUILayout.Button("Export\nData", GUILayout.Width(position.width / 12), GUILayout.Height(50)))
+            {
+                Debug.Log("Create a text file of data");
+            }
+            GUILayout.EndArea();
+
+            GUILayout.BeginArea(new Rect(position.width / 4, 80, (4 * position.width) / 3, position.height));
+            GUILayout.Box("My Box", GUILayout.Width((3 * position.width) / 4), GUILayout.Height(position.height - 80));
+            GUILayout.EndArea();
         }
     }
 }
